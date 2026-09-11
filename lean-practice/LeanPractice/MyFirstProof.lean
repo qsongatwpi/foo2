8f4593b5a3d2af56fd35964d38ab761863b86955
Qingshuo SONG <qsong@wpi.edu>
2026-09-11 10:47:09 -0400

    s

--- lean-practice/LeanPractice/MyFirstProof.lean
+++ lean-practice/LeanPractice/MyFirstProof.lean
@@ -3,3 +3,11 @@ example (a b : Nat) (h : a = b) : b = a := by
 
 example : 2 + 3 = 5 := by
   rfl
+
+example (a b c : Nat) : a + b + c = b + a + c := by
+  rw [Nat.add_comm a b]
+
+example (a b c : Nat) : a + b + c = a + c + b := by
+  rw [Nat.add_assoc]
+  rw [Nat.add_comm b c]
+  rw [← Nat.add_assoc]
